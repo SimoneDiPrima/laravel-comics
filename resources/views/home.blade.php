db8746549072c1cc44a9d0630da7dd4da6d2acf7
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-15 17:44:01 +0200

    creazione cartella includes e aggiunta header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,26 +9,7 @@
     <title>Laravel comics</title>
 </head>
 <body>
-    <div class="container d-flex justify-content-between align-items-center my-4">
-     <figure>
-            <img src="{{ asset('images/dc-logo.png') }}" alt="">
-    </figure>
-    <ul class="d-flex">
-            <li class="me-2">CHARACTERS</li>
-            <li class="me-2">COMICS</li>
-            <li class="me-2">MOVIES</li>
-            <li class="me-2">TV</li>
-            <li class="me-2">GAMES</li>
-            <li class="me-2">COLLECTIBLES</li>
-            <li class="me-2">VIDEO</li>
-            <li class="me-2">FANS</li>
-            <li class="me-2">NEWS</li>
-            <li class="me-2">SHOP</li>
-        </ul>
-    </div>
-    <div class="jumbotron img-fluid">
-        
-    </div>
+  @include('includes._header')
 
     
 </body>
