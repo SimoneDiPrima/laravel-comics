a4e4c21d5bba493b11d65c68fe2448ec71696267
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-15 17:25:43 +0200

    arcodamento header e aggiunta sass

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/app.css">
+    <title>Laravel comics</title>
+</head>
+<body>
+    <div class="container d-flex justify-content-between align-items-center my-4">
+     <figure>
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+    </figure>
+    <ul class="d-flex">
+            <li class="me-2">CHARACTERS</li>
+            <li class="me-2">COMICS</li>
+            <li class="me-2">MOVIES</li>
+            <li class="me-2">TV</li>
+            <li class="me-2">GAMES</li>
+            <li class="me-2">COLLECTIBLES</li>
+            <li class="me-2">VIDEO</li>
+            <li class="me-2">FANS</li>
+            <li class="me-2">NEWS</li>
+            <li class="me-2">SHOP</li>
+        </ul>
+    </div>
+    <div class="jumbotron img-fluid">
+        
+    </div>
+
+    
+</body>
+</html>
