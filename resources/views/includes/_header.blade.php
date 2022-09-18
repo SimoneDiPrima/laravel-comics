1cca5b39bd67992019319ca5db7db57503bc2ac0
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-09-18 17:20:38 +0200

    creazioni rotte relative a header menu

--- resources/views/includes/_header.blade.php
+++ resources/views/includes/_header.blade.php
@@ -18,14 +18,14 @@
          <img src="{{ asset('images/dc-logo.png') }}" alt="">
  </figure>
  <ul class="d-flex">
-         <li class="me-2"><a href="#" class="headerLink">CHARACTERS</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">COMICS</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">MOVIES</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">TV</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">GAMES</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">COLLECTIBLES</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">VIDEO</a></li>
-         <li class="me-2"><a href="#"  class="headerLink">FANS</a></li>
+         <li class="me-2"><a href="{{ route('characters') }}" class="headerLink">CHARACTERS</a></li>
+         <li class="me-2"><a href="{{ route('comics') }}"  class="headerLink">COMICS</a></li>
+         <li class="me-2"><a href="{{ route('movies') }}"  class="headerLink">MOVIES</a></li>
+         <li class="me-2"><a href="{{ route('tv') }}"  class="headerLink">TV</a></li>
+         <li class="me-2"><a href="{{ route('games') }}"  class="headerLink">GAMES</a></li>
+         <li class="me-2"><a href="{{ route('collectibles') }}"  class="headerLink">COLLECTIBLES</a></li>
+         <li class="me-2"><a href="{{ route('videos') }}"  class="headerLink">VIDEO</a></li>
+         <li class="me-2"><a href="{{ route('fans') }}"  class="headerLink">FANS</a></li>
          <li class="me-2"><a href="#"  class="headerLink">NEWS</a></li>
          <li class="me-2"><a href="#"  class="headerLink">SHOP</a></li>
      </ul>
